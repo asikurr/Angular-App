6918283eb02a12817dae8b367048e628d0ab3f28
asikurr <asikurrahman7@gmail.com>
2023-05-10 16:14:37 +0600

    created custom component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-ser',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{ }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+ // selector: 'app-servers', // this is user like - <app-servers></app-servers> in html file.
+ // selector: '[app-servers]',  // this is user like - <div app-servers></div> in html file.
+ selector: '.app-servers', // this is user like - <div class="app-servers"></div> in html file.
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
